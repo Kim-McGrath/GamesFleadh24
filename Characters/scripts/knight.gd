a3444993afd7dd86620f55459ba1e6454de79123
C21381381 Eduard Dravnieks <c21381381@mytudublin.ie>
2024-03-20 14:59:27 +0000

    Walking works again

--- Characters/scripts/knight.gd
+++ Characters/scripts/knight.gd
@@ -3,10 +3,6 @@ extends CharacterBody2D
 var enemy_in_attack_range = false
 var enemy_attack_cooldown = true
 var health = 10
-var player_alive = true
-
-var attack_ip = false #attack in progress
-
 @onready var animation_tree = $AnimationTree
 
 @onready var state_machine = animation_tree.get("parameters/playback")
@@ -15,14 +11,8 @@ var attack_ip = false #attack in progress
 @export var move_speed : float = 75
 
 func _physics_process(_delta):
-	enemy_attack()
-	update_health()
+	pass
 	
-	if health <= 0:
-		player_alive = false  # add endscreen
-		health = 0
-		print("Player died")
-		self.queue_free()
 
 	var input_direction = Vector2(
 		Input.get_action_strength("right") - Input.get_action_strength("left"),
@@ -37,48 +27,26 @@ func _physics_process(_delta):
 		$Sprite2D.flip_h = false
 	elif Input.is_action_pressed("left"):
 		$Sprite2D.flip_h = true
-
+	
 	pick_new_state()
 
 func pick_new_state():
 	if velocity != Vector2.ZERO:
+		print("Walking")
 		state_machine.travel("Walk")
 	if Input.is_action_pressed("mouse_left"):
 		# Ensure that the attack animation continues until it finishes
 		state_machine.travel("attack_right")
-		Global.player_current_attack = true
-		attack_ip = true
-		$deal_attack_timer.start()
-	else:
+	elif (velocity==Vector2.ZERO):
 		state_machine.travel("Idle")
 
 func player():
 	pass
 
-func _on_player_hit_box_body_entered(body):
-	if body.has_method("enemy"):
-		enemy_in_attack_range = true
-
-func _on_player_hit_box_body_exited(body):
-	if body.has_method("enemy"):
-		enemy_in_attack_range = false
-
-func enemy_attack():
-	if enemy_in_attack_range and enemy_attack_cooldown == true:
-		health = health - 1
-		enemy_attack_cooldown = false
-		$attack_cooldown.start()
-		print(health)
 
 
-func _on_attack_cooldown_timeout():
-	enemy_attack_cooldown = true
 
 
-func _on_deal_attack_timer_timeout():
-	$deal_attack_timer.stop()
-	Global.player_current_attack = false
-	attack_ip = false
 
 func update_health():
 	var healthbar = $HealthBar
